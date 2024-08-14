8a97af84ee463741d36fa85f87fa0860660fd9c2
RoshiniAjoy797 <Roshini@DESKTOP-2869A08>
2024-08-14 19:52:13 +0530

    Pattern - Pascal triangle

--- PatternProgramming/src/org/Jspider/Patterns/Ptn1.java
+++ PatternProgramming/src/org/Jspider/Patterns/Ptn1.java
@@ -1,14 +1,21 @@
 package org.Jspider.Patterns;
+
 import java.util.Scanner;
-public class Ptn1 {
+
+public class Ptn1 { // Pascal Triangle
 	public static void main(String[] args) {
-		Scanner sc= new Scanner(System.in);
-		System.out.println("Enter number of lines:");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number of lines: ");
 		int n = sc.nextInt();
-		for(int i= 0; i<n;i++)
-		{
-			System.out.print(" * ");
-			System.out.println("* ");
+		for (int i = 0; i <= n; i++) {
+			for (int j = 1; j <= n - i; j++)
+				System.out.print(" ");
+			int k = 1;
+			for (int j = 1; j <= i; j++) {
+				System.out.print(k + " ");
+				k = k * (i - j) / j;
+			}
+			System.out.println();
 		}
 		sc.close();
 	}
